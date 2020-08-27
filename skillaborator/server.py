38fe42cfc06d1ba7a13dfe30b98cc91dfa649e64
Zsolt Deák <lordmairtis@gmail.com>
2020-08-27 22:28:18 +0200

    feat(server): apply cors header for all requests

--- skillaborator/server.py
+++ skillaborator/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Response
 from flask_restful import Api
 from skillaborator.question import Question
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Question, '/question')
+
+
+@app.after_request
+def add_cors_after_request(response: Response):
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:4200'
+    return response
 
 
 if __name__ == '__main__':
