f880b01d09464cfa295e19ae950b67bf4208d7a6
Zsolt Deák <lordmairtis@gmail.com>
2021-03-06 11:01:52 +0100

    feat: previousQuestionIds from cookie instead of long appended query param list

--- skillaborator/question.py
+++ skillaborator/question.py
@@ -1,4 +1,5 @@
 from flask import Response, make_response
+from flask.json import loads, dumps
 from flask_restful import Resource, reqparse, abort
 
 from skillaborator.data_service import data_service
@@ -17,6 +18,7 @@ class Question(Resource):
         if 'code' in question:
             question['code']['value'] = question['code']['value'].replace('\\n', '\n').replace('\\t', '\t')
 
+    # TODO add 'neither' answer dynamically
     @staticmethod
     def get():
         """
@@ -29,14 +31,14 @@ class Question(Resource):
 
         parser.add_argument('answerId', dest='answerIds', type=str, help='Last question`s chosen answers',
                             action='append')
-        parser.add_argument('previousQuestionId', dest='previousQuestionIds', type=str,
-                            help='All previously answered questions', action='append')
+        parser.add_argument('previousQuestionIds', type=str,
+                            help='All previously answered questions', location='cookies')
 
         args = parser.parse_args(strict=True)
         current_score = args.get('currentScore')
         last_question_id = args.get('questionId')
         last_question_answers = args.get('answerIds')
-        previous_question_ids = args.get('previousQuestionIds') or []
+        previous_question_ids = loads(args.get('previousQuestionIds') or "[]")
 
         # answer received, calculate next score if can
         if last_question_answers is not None and len(last_question_answers) != 0:
@@ -47,6 +49,7 @@ class Question(Resource):
                                                                   current_score)
         else:
             current_score = 0
+            previous_question_ids = []
 
         next_level = ScoreService.calculate_next_question_level(current_score)
         # get random question on that level
@@ -55,11 +58,17 @@ class Question(Resource):
             Question.__no_question_found()
         Question.__format_question_texts(random_question)
 
+        previous_question_ids.append(random_question.get("id"))
+
         response: Response = make_response(random_question, 200)
         # TODO setcookie domain only for dev
         response.set_cookie(key='currentScore', value=f'{current_score}', httponly=True,
                             domain="api.app.localhost")
         response.set_cookie(key='questionId', value=f'{random_question.get("id")}', httponly=True,
+                            domain="api.app.localhost")
+        response.set_cookie(key='previousQuestionIds',
+                            value=f'{dumps(previous_question_ids, separators=(",", ":"))}',
+                            httponly=True,
                             domain="api.app.localhost")
 
         return response
