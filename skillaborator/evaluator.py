009bbc1bf67befe57c8e8c610310fa1f269d43f0
Zsolt Deák <lordmairtis@gmail.com>
2021-03-07 13:31:22 +0100

    refactor: evaluate get, use cookie data instead of custom body

--- skillaborator/evaluator.py
+++ skillaborator/evaluator.py
@@ -1,4 +1,5 @@
 from flask import Response
+from flask.json import loads
 from flask_restful import Resource, reqparse, abort
 
 from skillaborator.data_service import data_service
@@ -11,9 +12,9 @@ class Evaluator(Resource):
     def __need_proper_answers():
         abort(Response('Selected answers provided are not sufficient', status=400))
 
-    # TODO: save question and selected answer for analyzis
+    # TODO: save questions and selected answers for analysis
     @staticmethod
-    def post():
+    def get():
         """
         Post the selected answers for the questions, and get the right answers back in the response
         """
@@ -22,31 +23,22 @@ class Evaluator(Resource):
         parser.add_argument('currentScore', type=int, help='Current score of the user', location='cookies',
                             required=True)
         parser.add_argument('questionId', type=str, help='Last question`s id', location='cookies', required=True)
+        parser.add_argument('previousQuestionIds', type=str,
+                            help='All previously answered questions', location='cookies')
+        parser.add_argument('answerId', dest='answerIds', type=str, help='Last question`s chosen answers',
+                            action='append')
 
-        parser.add_argument('selectedAnswers', required=True, type=list, location="json")
         args = parser.parse_args(strict=True)
 
-        selected_answers = args.get('selectedAnswers')
         final_score = args.get('currentScore')
         last_question_id = args.get('questionId')
+        previous_question_ids = loads(args.get('previousQuestionIds'))
+        last_question_answers = args.get('answerIds')
 
-        answered_questions_ids = []
-        if selected_answers is None or final_score is None or last_question_id is None:
+        if last_question_answers is None or final_score is None or last_question_id is None:
             Evaluator.__need_proper_answers()
 
-        for index, selected_answer in enumerate(selected_answers):
-            question_id = selected_answer.get('questionId')
-            if question_id is None:
-                Evaluator.__need_proper_answers()
-            # get final score
-            if question_id == last_question_id:
-                selected_answer_ids = selected_answer.get('answerIds')
-                if selected_answer_ids is None:
-                    Evaluator.__need_proper_answers()
-                final_score = ScoreService.calculate_next_score(question_id, selected_answer_ids, final_score)
-            answered_questions_ids.append(question_id)
+        final_score = ScoreService.calculate_next_score(last_question_id, last_question_answers, final_score)
 
-        right_answers_by_question = data_service.get_questions_right_answers(answered_questions_ids)
-        for index, selected_answer in enumerate(selected_answers):
-            selected_answers[index]['rightAnswerIds'] = right_answers_by_question.get(selected_answer.get('questionId'))
-        return {"selectedAndRightAnswers": selected_answers, "score": final_score}, 200
+        right_answers_by_questions = data_service.get_questions_right_answers(previous_question_ids)
+        return {"rightAnswersByQuestions": right_answers_by_questions, "score": final_score}, 200
